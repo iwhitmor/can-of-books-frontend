80e90566c623759c9ece2b7b256ff3dbd47026b0
Ian Whitmore <ianwhitmor@gmail.com>
2021-09-14 16:22:41 -0500

    fix formatting on header.js

--- src/Header.js
+++ src/Header.js
@@ -8,12 +8,8 @@ class Header extends React.Component {
     return (
       <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
         <Navbar.Brand>My Favorite Books</Navbar.Brand>
-        <NavItem><Link to="/" className="nav-link">Home</Link></NavItem>
-        
-        
-        {this.props.user ?  <NavItem><Link to="/Profile" className="nav-link">Profile</Link></NavItem> : 
-          ''
-
+        <NavItem><Link to="/" className="nav-link">Home</Link></NavItem>  
+        {this.props.user ?  <NavItem><Link to="/Profile" className="nav-link">Profile</Link></NavItem> : ''
     }
         
        
