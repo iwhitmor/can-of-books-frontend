85244ddce4d6f823c978e4749fa9b639f5ad78e7
Ian Whitmore <ianwhitmor@gmail.com>
2021-09-15 11:06:05 -0500

    update formatting on header.js

--- src/Header.js
+++ src/Header.js
@@ -8,13 +8,11 @@ class Header extends React.Component {
     return (
       <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
         <Navbar.Brand>My Favorite Books</Navbar.Brand>
-        <NavItem><Link to="/" className="nav-link">Home</Link></NavItem>  
-        {this.props.user ?  <NavItem><Link to="/Profile" className="nav-link">Profile</Link></NavItem> : ''
-    }
-        
-       
-
-
+        <NavItem><Link to="/" className="nav-link">Home</Link></NavItem>
+        {this.props.user
+          ? <NavItem><Link to="/Profile" className="nav-link">Profile</Link></NavItem>
+          : ''
+        }
         {/* TODO: if the user is logged in, render the `LogoutButton` */}
       </Navbar>
     )
