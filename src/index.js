c4bc5dc6812a3e337b07978024f96ecdb1f36452
Ian Whitmore <ianwhitmor@gmail.com>
2021-09-19 21:37:34 -0500

    add auth0 context in index.js

--- src/index.js
+++ src/index.js
@@ -1,8 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App.js';
+import { Auth0Provider } from '@auth0/auth0-react';
 
 ReactDOM.render(
-  <App />,
+  <React.StrictMode>
+
+    <Auth0Provider
+      domain="dev-txxoephp.us.auth0.com"
+      clientId="YnQ55inNLqxYMUkyD2ZhUFGZH9eRfgzS"
+      redirectUri={window.location.origin}>
+
+      <App />
+    </Auth0Provider>
+  
+  </React.StrictMode >,
   document.getElementById("root")
 );
